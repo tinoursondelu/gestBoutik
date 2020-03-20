a40203e311802f2e36062f7d8ea3126484d65749
olivier <guiotolivier84120@gmail.com>
2020-03-20 10:40:47 +0100

    feature : first commit with bases

---
+++ src/main/java/com/Shop/GestBoutik/GestBoutikApplication.java
@@ -0,0 +1,13 @@
+package com.Shop.GestBoutik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestBoutikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestBoutikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Shop/GestBoutik/GestBoutikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shop.GestBoutik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestBoutikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
