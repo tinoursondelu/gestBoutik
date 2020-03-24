58082f834b8394ba5e61010763f7b36f401fedce
Steeve <thesliverbad@gmail.com>
2020-03-24 18:43:20 +0100

    Add ItemStoreDto

---
+++ src/main/java/com/Shop/GestBoutik/itemstore/ItemStoreDto.java
@@ -0,0 +1,23 @@
+package com.Shop.GestBoutik.itemstore;
+
+import com.Shop.GestBoutik.models.*;
+
+import java.util.Collection;
+
+public class ItemStoreDto {
+
+    private long id ;
+
+    private String designation ;
+
+    private Color color ;
+
+    private Size size ;
+
+    private Brand brand ;
+
+    private Collection<Store> stores ;
+
+    private Item item ;
+
+}
