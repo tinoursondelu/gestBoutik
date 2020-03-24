2d98e689f78331e58cf3ab48af3cec808fb45ac9
olivier <guiotolivier84120@gmail.com>
2020-03-24 11:55:01 +0100

    feature (store) : add methods in StoreServiceImpl

--- src/main/java/com/Shop/GestBoutik/store/StoreService.java
+++ src/main/java/com/Shop/GestBoutik/store/StoreService.java
@@ -1,5 +1,11 @@
 package com.Shop.GestBoutik.store;
 
+import java.util.Optional;
+
+import com.Shop.GestBoutik.models.Store;
+
 public interface StoreService {
+	
+	Optional<Store> findById(Long id);
 
 }
--- src/main/java/com/Shop/GestBoutik/store/StoreServiceImpl.java
+++ src/main/java/com/Shop/GestBoutik/store/StoreServiceImpl.java
@@ -2,6 +2,7 @@ package com.Shop.GestBoutik.store;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,47 +11,97 @@ import com.Shop.GestBoutik.models.Store;
 
 @Service
 public class StoreServiceImpl implements StoreService {
-	
+
 	@Autowired
 	private StoreRepository storeRepository;
-	
-	
-	
-	
 
-	
-	
-//	PARSE MODEL TO DTO
+	public void create(String designation) {
+
+		Store store = new Store();
+
+		store.setDesignation(designation);
+		storeRepository.save(store);
+	}
+
+	public void create(StoreDto storeDto) {
+
+		Store store = new Store();
+
+		store = parseDtoToModel(storeDto);
+		storeRepository.save(store);
+	}
+
+	public Store update(Long id, StoreDto storeDto) {
+
+		Store store = new Store();
+
+		Optional<Store> storeOpt = findById(id);
+		if (storeOpt.isPresent()) {
+			store = parseDtoToModel(storeDto);
+			storeRepository.save(store);
+		}
+		return store;
+	}
+
+
+
+	//	PPPPPPP		AAA		RRRRRR	  SSSSSSS	EEEEEEE
+	//	PP   PP	   AA AA	RR	 RR	  SS		EE
+	//	PP   PP	  AA   AA	RR	 RR	  SS		EE
+	//	PPPPPPP	  AAAAAAA	RRRRRR	  SSSSSSS	EEEEEE
+	//	PP		  AA   AA	RRRR	   	   SS	EE
+	//	PP		  AA   AA	RR	RR		   SS	EE
+	//	PP		  AA   AA	RR   RR	  SSSSSSS	EEEEEEE
+
+
+	//	PARSE MODEL TO DTO
 	public StoreDto parseModelToDto(Store store) {
-		
+
 		StoreDto storeDto = new StoreDto();
-		
+
 		storeDto.setId(store.getId());
 		storeDto.setDesignation(store.getDesignation());
-		
+
 		return storeDto;
 	}
-	
-//	PARSE LIST MODEL TO DTO
+
+	//	PARSE LIST MODEL TO DTO
 	public Collection<StoreDto> parseListModelToDto(Collection<Store> stores) {
-		
+
 		Collection<StoreDto> storesDto = new ArrayList<StoreDto>();
 		for (Store store : stores) {
 			storesDto.add(parseModelToDto(store));
 		}
-		
+
 		return storesDto;
 	}
-	
-//	PARSE DTO TO MODEL
+
+	//	PARSE DTO TO MODEL
 	public Store parseDtoToModel(StoreDto storeDto) {
-		
+
 		Store store = new Store();
-		
+
+		store.setId(storeDto.getId());
+		store.setDesignation(storeDto.getDesignation());
+
 		return store;
 	}
-	
-	
-	
+
+	//	DDDDD		BBBBBB
+	//	DD  DD		BB   BB
+	//	DD   DD		BB   BB
+	//	DD   DD		BBBBBB
+	//	DD   DD		BB   BB
+	//	DD  DD		BB   BB
+	//	DDDDD		BBBBBB
+
+	@Override
+	public Optional<Store> findById(Long id) {
+
+		return storeRepository.findById(id);
+	}
+
+
+
 
 }
